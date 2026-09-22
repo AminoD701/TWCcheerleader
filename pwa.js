f0f37d32e725c7a51fe6723c9037f32a60bad982
AminoD701 <ncdonalds3020@gmail.com>
2026-09-22 11:18:54 +0800

    Load girl default sorting enhancement

--- pwa.js
+++ pwa.js
@@ -172,7 +172,7 @@
     enhanceStandaloneMore();
   };
 
-  loadScriptOnce('./src/app/game-app-enhancements.js?v=6', 'game-app-enhancements');
+  loadScriptOnce('./src/app/girls-default-sort.js?v=1', 'girls-default-sort');\n  loadScriptOnce('./src/app/game-app-enhancements.js?v=6', 'game-app-enhancements');
   loadScriptOnce('./src/app/gacha-history.js?v=2', 'gacha-history');
   loadScriptOnce('./src/app/team-logo-overrides.js?v=8', 'team-logo-overrides');
   loadScriptOnce('./src/app/girl-career.js?v=4', 'girl-career');
